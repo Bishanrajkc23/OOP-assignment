2b690b8a85f376f1a8fe4257499aba6fc5b6d9fe
Bishanrajkc23 <bishanjung23@gmail.com>
2022-04-18 12:06:17 +0545

    ALLL

--- Hw3.java
+++ Hw3.java
@@ -5,8 +5,30 @@
 //        If more than 59 -> Upper second Class
 //        If more than 49 -> Second class
 //If more than 39 -> Third class and if below than 40 the result is fail
+import java.util.Scanner;
+
 public class Hw3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter MArk of 1st Subject: ");
+        double a = scanner.nextDouble();
+        System.out.print("Enter MArk of 2nd Subject: ");
+        double b = scanner.nextDouble();
+        System.out.print("Enter MArk of 3rd Subject: ");
+        double c = scanner.nextDouble();
+        System.out.print("Enter MArk of 4th Subject: ");
+        double d = scanner.nextDouble();
+        double total_mark;
+        double percentage;
+        String rank;
+        total_mark = a+b+c+d;
+        System.out.println("Total Marks is :" + total_mark);
+        percentage = total_mark/4;
+        System.out.println("Total Percentage is :" + percentage);
+        rank = (percentage>=70)?"First Class":(percentage>59)?"Upper Second Class":(percentage>49)?"Second Class":(percentage>39)?"Third Class":"Failed";
+        System.out.println("Your Grade is :" + rank);
     }
+}
 
 
 
--- Hw7.java
+++ Hw7.java
@@ -1,4 +1,17 @@
 //Take side of a square from user and print area and perimeter of it. Also, calculate SI, Area
 //        of triangle and Volume of Cube and Cuboid. Take the attributes as user input.
+import java.util.Scanner;
 public class Hw7 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter Length of Rectangle: ");
+        double a = scanner.nextDouble();
+        System.out.print("Enter Breadth of Rectangle: ");
+        double b = scanner.nextDouble();
+        double area;
+        area = a*b;
+        int area1 = (int)area;
+        System.out.println("The area of rectangle is: " +area1);
+    }
+
 }
